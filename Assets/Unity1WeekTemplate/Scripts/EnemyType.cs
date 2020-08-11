c23130fe630c3c2c90cf5d739a1abec0d9d96e61
Seiya-Ichikawa <seiya.ichikawa0628@gmail.com>
2020-08-11 12:45:39 +0900

    Add some elements and some scripts.

---
+++ Assets/Unity1WeekTemplate/Scripts/EnemyType.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyType : MonoBehaviour
+{
+    public enum Type { Normal, Hard, VeryHard };
+    public Type enemyType;
+}
