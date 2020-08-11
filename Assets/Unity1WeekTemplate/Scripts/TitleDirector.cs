fa3fa63e424979120725d42c3950669b4f26dcf8
Seiya-Ichikawa <seiya.ichikawa0628@gmail.com>
2020-08-11 11:11:02 +0900

    Add scene change fade effect.

---
+++ Assets/Unity1WeekTemplate/Scripts/TitleDirector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleDirector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeManager.Instance.LoadScene("MenuScene", 1.0f);
+            //効果音を鳴らす
+            //GetComponent<AudioSource>().Play();
+        }
+    }
+}
